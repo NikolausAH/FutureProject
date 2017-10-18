595c5bb9864ba5821843102b55d4f41b191feacd
DESKTOP-KPTRV25\BLI ARINANDA <arinanda.mail@gmail.com>
2017-10-18 23:33:07 +0700

    dropdown,more responsive, hamburger menu, rapikan code

---
+++ UI/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.menu').click(function(){
+    $('ul').toggleClass('active');
+  })
+})
+function closeNav() {
+    document.getElementById("dr-down").style.display="none";
+}
+function openNav() {
+    document.getElementById("dr-down").style.display="block";
+}
