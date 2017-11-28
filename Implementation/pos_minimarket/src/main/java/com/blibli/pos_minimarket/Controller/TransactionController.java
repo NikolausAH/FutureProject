a66500ed6059bf4900242a375d980719b50ddbb7
DESKTOP-NSE6UTN\BLI ARINANDA <arinanda.mail@gmail.com>
2017-11-28 22:33:56 +0700

    add product,stock link and start ui transaction

--- Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/Controller/TransactionController.java
+++ Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/Controller/TransactionController.java
@@ -21,52 +21,16 @@ public class TransactionController {
         transactionService.initTable();
         List<Product> productList;
         try {
-            productList=transactionService.getFromCart();
+            productList = transactionService.getFromCart();
             model.addAttribute("productList", productList);
             model.addAttribute("dateTime", transactionService.initDate());
-            model.addAttribute("transactionId",transactionService.initTransactionId());
-            model.addAttribute("tax",transactionService.initTax());
-            model.addAttribute("total",transactionService.initTotal());
-        }catch (Exception EX){
+            model.addAttribute("transactionId", transactionService.initTransactionId());
+            model.addAttribute("tax", transactionService.initTax());
+            model.addAttribute("total", transactionService.initTotal());
+        } catch (Exception EX) {
             System.out.println("Error TransactionController initialTransaction");
         }
         return "Transaction";
     }
-/*
-    @PostMapping(value = "/Transaction/addToCart")
-    public ModelAndView addToCart(@ModelAttribute("productId") Integer productId, @ModelAttribute("quantity") Integer quantity){
-        ModelAndView mav = new ModelAndView();
-        transactionService.addToCart(productId,quantity);
-        mav.setViewName("redirect:/Transaction");
-        return mav;
-    }
-
-//    @RequestMapping(value = "/Transaction/Add")
-//    public ModelAndView addTransaction(){
-////        ModelAndView mav = new ModelAndView();
-////        transactionService.addDetailTransaction();
-//        mav.setViewName("redirect:/Product");
-//        return mav;
-//    }
-//
-//    @RequestMapping("/Transaction/getProduct")
-//    public String addToCart(Model model, @ModelAttribute("productId") Integer productId, @ModelAttribute("quantity") Integer quantity) {
-//        Product product;
-//        List<Product> productList = new ArrayList<>();
-//        try {
-//            product=productService.getById(productId);
-//            product.setQuantity(quantity);
-//            System.out.println(product.getProductId());
-//            System.out.println(product.getQuantity());
-//            productList.add(product);
-//            model.addAttribute("productList", productList);
-//        }catch (Exception EX){
-//            System.out.println("Error TransactionController Transaction/getProduct");
-//            System.out.println(EX.toString());
-//        }
-//        return "redirect:/Transaction";
-//    }
-
-*/
 
 }
