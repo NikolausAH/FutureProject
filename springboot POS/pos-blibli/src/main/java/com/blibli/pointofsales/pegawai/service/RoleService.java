18cce3ef963027376b5a4c5a6d7ebcd00d05aeee
Nikolaus Aldo Halim <Nikolausaldoh@gmail.com>
2017-11-30 21:40:54 +0700

    hapus

--- springboot POS/pos-blibli/src/main/java/com/blibli/pointofsales/pegawai/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.blibli.pointofsales.pegawai.service;
-
-import com.blibli.pointofsales.pegawai.model.entity.Role;
-
-import java.util.List;
-
-public interface RoleService {
-    List<Role> findAll(); //ambil semua data di tabel pegawai
-}
