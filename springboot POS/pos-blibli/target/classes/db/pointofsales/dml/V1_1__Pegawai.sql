10f238ccbbfab39cd1d151b3be411951fef02f7b
Nikolaus Aldo Halim <Nikolausaldoh@gmail.com>
2017-11-30 19:57:12 +0700

    no message

---
+++ springboot POS/pos-blibli/target/classes/db/pointofsales/dml/V1_1__Pegawai.sql
@@ -0,0 +1,3 @@
+--dijalanin di flyway di application properties--
+INSERT INTO role (id,nama_role) VALUES ('89734fee-30f3-4160-ab37-98721d6f975b','Kasir');
+INSERT INTO role (id,nama_role) VALUES ('7944bd4e-782d-4ea2-adf7-a25a717422f7','Manager');
