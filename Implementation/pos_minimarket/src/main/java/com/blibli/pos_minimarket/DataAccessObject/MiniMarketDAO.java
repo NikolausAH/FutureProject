45dbb854f1c89d8027d6566c21eaf21b742b30bb
Nikolaus Aldo Halim <Nikolausaldoh@gmail.com>
2017-11-30 22:10:50 +0700

    push minimarketDAO

--- Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/DataAccessObject/MiniMarketDAO.java
+++ Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/DataAccessObject/MiniMarketDAO.java
@@ -1,32 +1,64 @@
+
 package com.blibli.pos_minimarket.DataAccessObject;
 
-import java.sql.PreparedStatement;
+import com.blibli.pos_minimarket.Model.Minimarket;
+
 import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class MiniMarketDAO extends ConnectionSettings {
-    public MiniMarketDAO() {
-    }
-
-    public Double getTax(){
-        Double tax = 0.0;
-        String sql = "SELECT tax FROM mini_market;";
-        String message = "Error TransactionDAO getNextId";
-        try {
-            this.makeConnection();
-            PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet != null)  {
-                while (resultSet.next()){
-                    tax = resultSet.getDouble("tax");
-                }
-            }
-            this.closeConnection();
+public class MinimarketDAO extends MyConnection{
+    private void inisialisasiMinimarket(){
+        String sql = "INSERT INTO minimarket (nama,address,nomortelp, email, tax, receiptdesc) values('blibli', 'jalan xxx', '087885172045', 'blibli@gmail.com', 30, 'keterangan receipt');";
+        try{
+            Statement state = c.createStatement();
+            state.executeQuery(sql);
+            state.close();
         }
-        catch (Exception EX){
-            System.out.println(message);
+        catch(Exception EX){
+            System.out.println("Error MinimarketDAO inisialisasiMinimarket");
             System.out.println(EX.toString());
         }
-        tax /=100;
-        return tax;
     }
+    private void update(Minimarket minimarket){
+        String nama  = minimarket.getName();
+        String address = minimarket.getAddress();
+        String telephoneNumber = minimarket.getTelephoneNumber();
+        String email = minimarket.getEmail();
+        int tax = minimarket.getTax();
+        String receiptText = minimarket.getReceiptDesc();
+
+        String sql = "UPDATE minimarket SET nama ='"+nama+"',address='"+address+"',nomortelp='"+telephoneNumber+"',email='"+email+"',tax='"+tax+"',receiptdesc='"+receiptText+"';";
+        try{
+            Statement state = c.createStatement();
+            state.executeUpdate(sql);
+            state.close();
+        } catch (Exception EX) {
+            System.out.println("Error MinimarketDAO update");
+            System.out.println(EX.toString());
+        }
+    }
+    public void updateMinimarket(Minimarket minimarket) {
+
+        String sql = "select * from minimarket";
+        try {
+            //c koneksi global dari extends myConn
+            Statement state = c.createStatement();
+            ResultSet rs = state.executeQuery(sql);
+            if (rs == null) {
+
+                inisialisasiMinimarket();
+            }
+
+            else{
+                update(minimarket);
+            }
+            state.close();
+
+        } catch (Exception EX) {
+            System.out.println("Error CategoryDAO getAllCategory");
+            System.out.println(EX.toString());
+        }
+
+    }
+
 }
