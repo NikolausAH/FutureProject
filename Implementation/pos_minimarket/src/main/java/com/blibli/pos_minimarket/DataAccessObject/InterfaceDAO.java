be205af59f6635ede9bfe6ef857b76505b540f2a
DESKTOP-NSE6UTN\BLI ARINANDA <arinanda.mail@gmail.com>
2017-11-30 22:12:06 +0700

    head

--- Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/DataAccessObject/InterfaceDAO.java
+++ Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/DataAccessObject/InterfaceDAO.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 public interface InterfaceDAO<objectType,idType,keyType> {
     void initTable();
+
     List<objectType> getAll();
+
     objectType getById(idType key);
+
     List<objectType> search(keyType key);
-   // public objectType getOne(idType idType);
+
+    // public objectType getOne(idType idType);
     void add(objectType t);
+
     void update(objectType t);
+
     void delete(idType id);
+
     void softDelete(idType idType);
-<<<<<<< HEAD
-
-
 }
-=======
-//
-}
->>>>>>> spring
