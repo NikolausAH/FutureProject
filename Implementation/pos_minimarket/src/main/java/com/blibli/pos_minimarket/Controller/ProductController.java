3cca6bb7501b4da95e87ab2e0eb41fee6b08529d
DESKTOP-NSE6UTN\BLI ARINANDA <arinanda.mail@gmail.com>
2017-12-30 13:29:55 +0700

    skema update delete into new page, modal add, search at Product, clear all file css

--- Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/Controller/ProductController.java
+++ Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/Controller/ProductController.java
@@ -6,9 +6,7 @@ import com.blibli.pos_minimarket.Services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,23 +23,30 @@ public class ProductController {
     @RequestMapping("/Product")
     public String showAllProduct(@ModelAttribute("searchKey")String searchKey, Model model) {
         productService.initTable();
-        model.addAttribute("product", productService.search(searchKey));
+        model.addAttribute("productList", productService.search(searchKey));
         model.addAttribute("categoryList", categoryService.showAll());
-        model.addAttribute("product_nextId", categoryService.getNextId());
+        model.addAttribute("product_nextId", productService.getNextId());
         return "Product";
+    }
+
+    @RequestMapping(value = "/product/detail/{productId}", method = RequestMethod.GET)
+    public String detailEmployee(@PathVariable Integer productId, Model model){
+        model.addAttribute("categoryList", categoryService.showAll());
+        model.addAttribute("product", productService.getById(productId));
+        return "ProductDetail";
     }
 
     @PostMapping(value = "/Product/Add")
     public ModelAndView createProduct(@ModelAttribute("product") Product product,@ModelAttribute("categoryId")Integer categoryId){
         ModelAndView mav = new ModelAndView();
         product.setCategory(categoryService.getById(categoryId));
-        product.setProductId(categoryService.getNextId());
+        product.setProductId(productService.getNextId());
         productService.add(product);
         mav.setViewName("redirect:/Product");
         return mav;
     }
 
-    @PostMapping(value = "/Product/Update")
+    @RequestMapping(value = "/Product/Detail", params = "update", method = RequestMethod.POST)
     public ModelAndView updateProduct(@ModelAttribute("product") Product product,@ModelAttribute("categoryId")Integer categoryId){
         ModelAndView mav = new ModelAndView();
         product.setCategory(categoryService.getById(categoryId));
@@ -50,12 +55,17 @@ public class ProductController {
         return mav;
     }
 
-    @RequestMapping(value = "/Product/Delete")
+    @RequestMapping(value = "/Product/Detail", params = "delete", method = RequestMethod.POST)
     public ModelAndView deleteProduct(@ModelAttribute("productId")Integer productId){
         ModelAndView mav = new ModelAndView();
         productService.softDelete(productId);
         mav.setViewName("redirect:/Product");
         return mav;
+    }
+
+    @RequestMapping(value = "/Product/Detail",params = "cancel", method = RequestMethod.POST)
+    public String cancelCategory(){
+        return "redirect:/Product";
     }
 
 }
