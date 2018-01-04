a4c99c6a29e3adb70d11ba5410f8f7e43791ad72
Nikolaus Aldo Halim <Nikolausaldoh@gmail.com>
2018-01-05 04:48:47 +0700

    fix bug

--- Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/Controller/TransactionController.java
+++ Implementation/pos_minimarket/src/main/java/com/blibli/pos_minimarket/Controller/TransactionController.java
@@ -1,5 +1,6 @@
 package com.blibli.pos_minimarket.Controller;
 
+import com.blibli.pos_minimarket.Model.Employee;
 import com.blibli.pos_minimarket.Model.TransactionDetail;
 import com.blibli.pos_minimarket.Services.TransactionDetailService;
 import com.blibli.pos_minimarket.Services.TransactionService;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -21,7 +23,13 @@ public class TransactionController {
     private TransactionDetailService transactionDetailService =  new TransactionDetailService();
 
     @RequestMapping("/Transaction")
-    public String initialTransaction(Model model) {
+    public String initialTransaction(HttpServletRequest request,Model model) {
+        Employee employee = (Employee) request.getSession().getAttribute("pegawai");
+        model.addAttribute("pegawai", employee);
+        if (employee == null) {
+            return "Login";
+        }
+        else if(employee.getRole().getName().equals("Kasir")){
         transactionService.initTable();
         List<TransactionDetail> transactionDetailList;
         try {
@@ -38,7 +46,25 @@ public class TransactionController {
         } catch (Exception EX) {
             System.out.println("Error TransactionController initialTransaction");
         }
-        return "Transaction";
+        return "Cashier";}
+        else{
+            transactionService.initTable();
+            List<TransactionDetail> transactionDetailList;
+            try {
+                transactionDetailList = transactionService.getAllFromCart();
+                Double total = transactionService.getTotal(transactionDetailList);
+                Double discountTotal = transactionService.getDiscountTotal(total,transactionService.initDate());
+                Double tax = transactionService.getTax(total-discountTotal);
+                model.addAttribute("transactionDetailList", transactionDetailList);
+                model.addAttribute("date_Time", transactionService.initDate());
+                model.addAttribute("transaction_nextId", transactionService.getNextId());
+                model.addAttribute("tax", tax);
+                model.addAttribute("total",total);
+//            model.addAttribute("discountTotal",discountTotal);
+            } catch (Exception EX) {
+                System.out.println("Error TransactionController initialTransaction");
+            }
+            return "Transaction";}
     }
 
     @PostMapping(value = "Transaction/addToCart")
